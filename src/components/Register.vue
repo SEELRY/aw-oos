78ffde6fe73d4913b6b26d3a5db3029db7860bf7
Arthur <wang@xunnn.com>
2019-05-28 20:56:14 +0800

    登录和注册页面结构搭建

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,3 +1,50 @@
 <template>
-    <h1>Register</h1>
+    <div class="row mt-3">
+        <div class="col-md-12 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <img class="mx-auto d-block xl-log" src="../../src/assets/icon.png" alt="">
+                    <form @submit.prevent="onSubmit">
+                        <div class="form-group">
+                            <label for="email">邮箱</label>
+                            <input type="email" class="form-control" v-model="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">密码</label>
+                            <input type="password" class="form-control" v-model="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmPassword">确认密码</label>
+                            <input type="confirmPassword" class="form-control" v-model="email">
+                        </div>
+                        <button class="btn btn-block btn-success">注册</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    },
+    methods:{
+        onSubmit(){
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+.xl-log{
+    width: 300px;
+}
+</style>
+
