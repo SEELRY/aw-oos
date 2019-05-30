4e4c5eb787c2a0efc37a71c8cf814b7257da375d
Arthur <wang@xunnn.com>
2019-05-30 18:31:49 +0800

    添加点餐菜单样式，添加购物车样式。

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,7 +11,7 @@
                         </div>
                         <div class="form-group">
                             <label for="password">密码</label>
-                            <input type="password" class="form-control" v-model="email">
+                            <input type="password" class="form-control" v-model="password">
                         </div>
                         <button class="btn btn-block btn-success">登录</button>
                     </form>
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data(){
         return {
@@ -31,6 +32,33 @@ export default {
     },
     methods:{
         onSubmit(){
+            // console.log("test");
+            axios.get('/users.json')
+            .then(res => {
+                // console.log(res.data);
+                const data = res.data;
+                const users = [];
+                for(let key in data){
+                    const user = data[key];
+                    // console.log(user);
+                    users.push(user);
+                }
+                // console.log(users);
+
+                //实现过滤
+                let result = users.filter((user) => {
+                    return user.email === this.email && user.password === this.password;
+                });
+                // console.log(result);
+
+                //判断result长度是否大于0
+                if(result != null && result.length >0){
+                    alert("登录成功！");
+                    this.$router.push({name:"homeLink"});
+                }else{
+                    alert("帐号或密码错误，请核对后输入。");
+                }
+            })
             
         }
     }
