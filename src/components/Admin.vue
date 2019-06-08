7ff7c35a3e26a14607f5046b1b2aeb77bf710b29
Arthur Wang <wang@xunnn.com>
2019-06-08 17:13:12 +0800

    构建admin页面

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,27 +1,37 @@
 <template>
-    <h1>Admin</h1>
+    <div class="row">
+        <div class="col-sm-12 col-md-8">
+            <!-- new pizza -->
+            <app-new-pizza></app-new-pizza>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <!-- 品种展示 -->
+            <h3 class="text-muted my-5">菜单</h3>
+            <table class="table">
+                <thead class="table table-default">
+                    <tr>
+                        <th>品种</th>
+                        <th>删除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>榴莲pizza</td>
+                        <td>
+                            <button class="btn btn-outline-danger btn-sm">&times;</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 <script>
+import NewPizza from './NewPizza.vue'
 export default {
-    data(){
-        return {
-            name:"Arthur"
-        }
-    },
-    // beforeRouteEnter:(to,from,next)=>{
-    //     // alert("Hello" + this.name);
-    //     // next();
-
-    //     next(vm=>{
-    //         alert("Hello " + vm.name);
-    //     })
-    // },
-    // beforeRouteLeave:(to,from,next)=>{
-    //     if(confirm("确定离开吗？") == true){
-    //         next();
-    //     }else{
-    //         next(false);
-    //     }
-    // }
+    components:{
+        'app-new-pizza':NewPizza
+    }
 }
 </script>
+
