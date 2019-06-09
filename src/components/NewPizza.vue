8fa795d4423449d4dfed500b352ac5433d756230
Arthur <wang@xunnn.com>
2019-06-10 06:59:12 +0800

    实现新增数据到库

--- src/components/NewPizza.vue
+++ src/components/NewPizza.vue
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="" class="col-sm-1">描述</label>
+            <label class="col-sm-1">描述</label>
             <div class="col-sm-11">
                 <textarea rows="5" class="form-control" v-model="newPizza.description">
                 </textarea>
@@ -16,32 +16,32 @@
         </div>
         <p><strong>选项1</strong></p>
         <div class="form-group row">
-            <label for="" class="col-sm-1">尺寸</label>
+            <label class="col-sm-1">尺寸</label>
             <div class="col-sm-11">
                 <input type="text" class="form-contorl" v-model="newPizza.size1">
             </div>
         </div>
         <div class="form-group row">
-            <label for="" class="col-sm-1">价格</label>
+            <label class="col-sm-1">价格</label>
             <div class="col-sm-11">
                 <input type="text" class="form-contorl" v-model="newPizza.price1">
             </div>
         </div>
         <p><strong>选项2</strong></p>
         <div class="form-group row">
-            <label for="" class="col-sm-1">尺寸</label>
+            <label class="col-sm-1">尺寸</label>
             <div class="col-sm-11">
                 <input type="text" class="form-contorl" v-model="newPizza.size2">
             </div>
         </div>
         <div class="form-group row">
-            <label for="" class="col-sm-1">价格</label>
+            <label class="col-sm-1">价格</label>
             <div class="col-sm-11">
                 <input type="text" class="form-contorl" v-model="newPizza.price2">
             </div>
         </div>
         <div class="form-group row">
-            <button type="button" class="btn btn-success btn-block">添加</button>
+            <button @click="addMenuItem" type="button" class="btn btn-success btn-block">添加</button>
         </div>
     </form>
 </template>
@@ -51,6 +51,42 @@ export default {
         return {
             newPizza:{},
         }
+    },
+    methods:{
+        addMenuItem(){
+            // console.log(this.newPizza);
+            let data = {
+                name:this.newPizza.name,
+                description:this.newPizza.description,
+                options:[
+                    {
+                        size:this.newPizza.size1,
+                        price:this.newPizza.price1
+                    },
+                    {
+                        size:this.newPizza.size2,
+                        price:this.newPizza.price2
+                    }
+                ]
+            }
+
+            //axios vue-resource
+            //es6 fetch
+            fetch("https://pizza-69e10.firebaseio.com/menu.json",{
+                method:"POST",
+                headers:{
+                    "Content-type":'application/json'
+                },
+                body:JSON.stringify(data)
+            })
+            // .then(res => console.log(res))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .then(data => this.$router.push({name:"menuLink"}))
+            .catch(err => console.log(err));
+
+        }
+
     }
 }
 </script>
