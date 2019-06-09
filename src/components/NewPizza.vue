bf637439044afc368fae8b7425c02e43ae4a4cd5
Arthur <wang@xunnn.com>
2019-06-09 22:29:47 +0800

    布局管理页面

---
+++ src/components/NewPizza.vue
@@ -0,0 +1,57 @@
+<template>
+    <form>
+        <h3 class="text-muted my-5">添加新品</h3>
+        <div class="form-group row">
+            <label class="col-sm-1">品种</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-contorl" v-model="newPizza.name">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">描述</label>
+            <div class="col-sm-11">
+                <textarea rows="5" class="form-control" v-model="newPizza.description">
+                </textarea>
+            </div>    
+        </div>
+        <p><strong>选项1</strong></p>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">尺寸</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-contorl" v-model="newPizza.size1">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">价格</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-contorl" v-model="newPizza.price1">
+            </div>
+        </div>
+        <p><strong>选项2</strong></p>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">尺寸</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-contorl" v-model="newPizza.size2">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">价格</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-contorl" v-model="newPizza.price2">
+            </div>
+        </div>
+        <div class="form-group row">
+            <button type="button" class="btn btn-success btn-block">添加</button>
+        </div>
+    </form>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            newPizza:{},
+        }
+    }
+}
+</script>
+
